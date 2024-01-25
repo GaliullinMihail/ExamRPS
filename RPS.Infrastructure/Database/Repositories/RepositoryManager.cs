2f5a7e7a44409760b72248ed5f752a25f6d4f9b1
AndrejHoryakov <andrejhorakov47232@gmail.com>
2024-01-25 16:36:08 +0300

    [rm] old code

--- RPS.Infrastructure/Database/Repositories/RepositoryManager.cs
+++ RPS.Infrastructure/Database/Repositories/RepositoryManager.cs
@@ -4,16 +4,13 @@ namespace RPS.Infrastructure.Database.Repositories;
 
 public sealed class RepositoryManager : IRepositoryManager
 {
-    private readonly Lazy<IUnitOfWork> _lazyUnitOfWork;
     private readonly Lazy<IRoomRepository> _lazyRoomRepository;
     private readonly Lazy<IMessageRepository> _lazyMessageRepository;
     public RepositoryManager(ApplicationDbContext dbContext)
     {
-        _lazyUnitOfWork = new Lazy<IUnitOfWork>(() => new UnitOfWork(dbContext));
         _lazyMessageRepository = new Lazy<IMessageRepository>(() => new MessageRepository(dbContext));
         _lazyRoomRepository = new Lazy<IRoomRepository>(() => new RoomRepository(dbContext));
     }
-    public IUnitOfWork UnitOfWork => _lazyUnitOfWork.Value;
     public IRoomRepository RoomRepository => _lazyRoomRepository.Value;
     public IMessageRepository MessageRepository => _lazyMessageRepository.Value;
 }
